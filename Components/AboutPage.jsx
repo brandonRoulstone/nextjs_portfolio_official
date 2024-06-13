17eb572c9496d2448f7b33f4d6a66be462dd39fb
Brandon <brandonroulstone71@gmail.com>
2024-06-13 12:40:58 +0200

    image...

--- Components/AboutPage.jsx
+++ Components/AboutPage.jsx
@@ -4,7 +4,7 @@ import Item from "./SkillsetIcons";
 import Image from "next/image";
 const AboutPage = () => {
     const { scrollYProgress } = useScroll();
-    const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+    const scale = useTransform(scrollYProgress, [0, 1], [0.2, 1]);
   return (
     <>
       <div className="min-h-screen flex justify-center items-center flex-row w-[100%]">
@@ -16,12 +16,12 @@ const AboutPage = () => {
             About Me
           </div>
         </div>
-        <div className="w-[100%] flex justify-center min-h-screen items-center">
+        <motion.div style={{scale}} className="w-[100%] flex justify-center min-h-screen items-center">
           <Image src="https://cdn-images.imagevenue.com/99/06/5a/ME18HJDQ_o.jpg" height={400} width={400} alt="" />
         {/* <div className="container py-5">
           <Item />
         </div> */}
-        </div>
+        </motion.div>
       </div>
     </>
   )
